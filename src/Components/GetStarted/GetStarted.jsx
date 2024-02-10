68399302689d5d1325c029063e04feed22374891
Samson Ameh <amehsam69@gmail.com>
2024-02-10 17:55:14 +0100

    style getstarted

--- src/Components/GetStarted/GetStarted.jsx
+++ src/Components/GetStarted/GetStarted.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import styles from "./GetStarted.module.scss";
 
 const GetStarted = () => {
   return (
-    <section>
-      <div>
-        <span>We Can Help </span>
-        <h2>Let's show you how we can use tech to grow your business !</h2>
-      </div>
+    <section className={styles.getStarted}>
+      <div className={styles.getStartedContainer}>
+        <div className={styles.content}>
+          <span className={styles.intro}>We Can Help </span>
+          <h2 className={styles.title}>
+            Let's show you how we can use tech <br /> to grow your business !
+          </h2>
+        </div>
 
-      <div>
-        <a href="">Get Started Now</a>
+        <div>
+          <a href="">Get Started Now</a>
+        </div>
       </div>
     </section>
   );
