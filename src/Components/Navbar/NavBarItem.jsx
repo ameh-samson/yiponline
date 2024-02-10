af3f98a7c84a53741c80f20f4547f8993df2514a
Samson Ameh <amehsam69@gmail.com>
2024-02-10 04:49:17 +0100

    style nav

--- src/Components/Navbar/NavBarItem.jsx
+++ src/Components/Navbar/NavBarItem.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import styles from "./NavBarItem.module.scss";
 
 const NavBarItem = () => {
   return (
     <>
-      <li>Home</li>
-      <li>Products</li>
-      <li>Careers</li>
-      <li>Contact Us</li>
-      <li>Join Us</li>
+      <li className={styles.navLink}>
+        <a href="">Home </a>
+      </li>
+      <li className={styles.navLink}>
+        <a href="">Products</a>
+      </li>
+      <li className={styles.navLink}>
+        <a href="">Careers </a>
+      </li>
+      <li className={styles.navLink}>
+        <a href="">Contact Us</a>
+      </li>
+      <li className={`${styles.navLink} ${styles.joinUs}`}>
+        <a href="">Join Us </a>
+      </li>
     </>
   );
 };
