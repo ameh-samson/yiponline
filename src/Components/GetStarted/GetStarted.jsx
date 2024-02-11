09028552e80563e008fda9f57b5b0d76d29942d8
Samson Ameh <amehsam69@gmail.com>
2024-02-11 09:55:57 +0100

    remove <br>

--- src/Components/GetStarted/GetStarted.jsx
+++ src/Components/GetStarted/GetStarted.jsx
@@ -7,7 +7,7 @@ const GetStarted = () => {
         <div className={styles.content}>
           <span className={styles.intro}>We Can Help </span>
           <h2 className={styles.title}>
-            Let's show you how we can use tech <br /> to grow your business !
+            Let's show you how we can use tech to grow your business !
           </h2>
         </div>
 
