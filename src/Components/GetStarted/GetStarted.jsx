332e3b6935db411869156f362998c6949a1536f6
Samson Ameh <amehsam69@gmail.com>
2024-02-14 12:19:51 +0100

    add to getStarted

--- src/Components/GetStarted/GetStarted.jsx
+++ src/Components/GetStarted/GetStarted.jsx
@@ -1,18 +1,43 @@
 import styles from "./GetStarted.module.scss";
+import { useInView } from "react-intersection-observer";
 
 const GetStarted = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
+  const [ref2, inView2] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
   return (
     <section className={styles.getStarted}>
       <div className={styles.getStartedContainer}>
         <div className={styles.content}>
-          <span className={styles.intro}>We Can Help </span>
-          <h2 className={styles.title}>
+          <span
+            ref={ref}
+            className={`${styles.intro} ${inView ? styles.appear : ""}`}
+          >
+            We Can Help{" "}
+          </span>
+          <h2
+            ref={ref2}
+            className={`${styles.title} ${inView2 ? styles.appear : ""}`}
+          >
             Let's show you how we can use tech to grow your business !
           </h2>
         </div>
 
         <div>
-          <a className={styles.ctaGetStarted} href="">
+          <a
+            ref={ref2}
+            className={`${styles.ctaGetStarted} ${
+              inView2 ? styles.appear : ""
+            }`}
+            href=""
+          >
             Get Started Now
           </a>
         </div>
