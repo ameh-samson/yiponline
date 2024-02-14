79d26abed88c981c156accce8d83dc342fea4ff6
Samson Ameh <amehsam69@gmail.com>
2024-02-14 19:56:27 +0100

    scrollToTop component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
 import "./App.scss";
 import Layout from "./Components/Layout/Layout";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Products from "./Pages/Products/Products";
 import Home from "./Pages/Home/Home";
 import Careers from "./Pages/Careers/Careers";
 import ContactUs from "./Pages/ContactUs/ContactUs";
 import SignUp from "./Pages/SignUp/SignUp";
+import ScrollToTop from "./Components/Util/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/career" element={<Careers />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Route>
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/career" element={<Careers />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
+        </Routes>
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
