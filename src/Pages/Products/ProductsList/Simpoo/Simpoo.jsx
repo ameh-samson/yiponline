f50e0621719ff75d77a961f3967967493e2db32f
Samson Ameh <amehsam69@gmail.com>
2024-02-14 21:02:05 +0100

    adds simpo img

--- src/Pages/Products/ProductsList/Simpoo/Simpoo.jsx
+++ src/Pages/Products/ProductsList/Simpoo/Simpoo.jsx
@@ -1,9 +1,11 @@
 import Card from "../../../../Components/Card/Card";
+import simpoImg from "../../../../assets/SimpooBooks.png";
 
 const Simpoo = () => {
   return (
     <>
       <Card
+        imageUrl={simpoImg}
         title="Simpo"
         content="Simpoo effortlessly keeps track of financial transactions, handles billing for clients, and facilitates online payments. 
         Every business needs to make bookkeeping Simpoo!"
