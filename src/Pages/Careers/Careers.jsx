1a73ba18c797fe351d3b15dff826d8359278d858
Samson Ameh <amehsam69@gmail.com>
2024-02-15 14:42:33 +0100

    add career content

--- src/Pages/Careers/Careers.jsx
+++ src/Pages/Careers/Careers.jsx
@@ -1,5 +1,37 @@
+import styles from "./Careers.module.scss";
+import teamImg from "../../assets/team.jpeg";
+
 const Careers = () => {
-  return <div>Careers</div>;
+  return (
+    <section className={styles.career}>
+      <div className={styles.careerContainer}>
+        <div className={styles.careerTitleContainer}>
+          <h1 className={`${styles.productTitle} `}>Careers</h1>
+        </div>
+
+        <div className={styles.careerContent}>
+          <span>Our Products & Services</span>
+          <h2>Be part of our team</h2>
+          <p>
+            Building accessible tech to structure, automate & scale-up African
+            businesses!
+          </p>
+        </div>
+
+        <div>
+          <img src={teamImg} alt="" />
+        </div>
+
+        <div className={styles.employeeCulture}>
+          <h2>Our Employee Culture</h2>
+          <p>
+            Integrity, Kindness, Curiosity & Excellence We make room for the
+            best talent! Check us out!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Careers;
